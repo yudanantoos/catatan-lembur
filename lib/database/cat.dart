58cf798ff01dc525ee8e62d14775d97bc4144c50
yudanantoos <yudanantoos@gmail.com>
2021-01-28 11:36:20 +0700

    convert a Cat into a map

--- lib/database/cat.dart
+++ lib/database/cat.dart
@@ -4,4 +4,14 @@ class Cat {
   final String note;
 
   Cat(this.id, this.dateTime, this.note);
+
+  // Convert a Cat into a map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id' : id,
+      'date_time' : dateTime,
+      'note' : note,
+    };
+  }
 }
