4258bb9fbb6b03da4323c58175072edae98807ce
yudanantoos <yudanantoos@gmail.com>
2021-01-29 22:16:40 +0700

    add getters

--- lib/database/cat.dart
+++ lib/database/cat.dart
@@ -1,19 +1,25 @@
 class Cat {
-  final int id;
+  final int _id;
   final double _actHours, _totalHours;
   final String _dateTime, _note;
 
-  Cat(this.id, this._actHours, this._totalHours, this._dateTime, this._note);
+  Cat(this._id, this._actHours, this._totalHours, this._dateTime, this._note);
 
   // Convert a Cat into a map. The keys must correspond to the names of the
   // columns in the database.
   Map<String, dynamic> toMap() {
     return {
-      'id' : id,
+      'id' : _id,
       'act_hours' : _actHours,
       'total_hours' : _totalHours,
       'date_time' : _dateTime,
       'note' : _note,
     };
   }
+
+  get id => _id;
+  get actHours => _actHours;
+  get totalHours => _totalHours;
+  get dateTime => _dateTime;
+  get note => _note;
 }
