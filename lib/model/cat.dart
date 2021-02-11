b85b9e7abcffcdaf130468be4347000612f7f338
yudanantoos <yudanantoos@gmail.com>
2021-02-11 15:45:01 +0700

    add getters & setters

--- lib/model/cat.dart
+++ lib/model/cat.dart
@@ -1,40 +1,76 @@
 class Cat {
-  int id;
-  double actHours, totalHours, overtimePay;
-  String dateTime, note;
+  int _id;
+  double _actHours, _totalHours, _overtimePay;
+  String _dateTime, _note;
 
   static const tblCats = 'cats';
-  static const colId = 'id';
+  static const colId = '_id';
   static const colActHours = 'act_hours';
   static const colTotalHours = 'total_hours';
   static const colOvertimePay = 'overtime_pay';
   static const colDateTime = 'date_time';
   static const colNote = 'note';
 
-  Cat({this.id, this.actHours, this.totalHours, this.dateTime, this.note});
+  Cat(this._id, this._actHours, this._totalHours, this._overtimePay, this._dateTime, this._note);
 
   Cat.fromMap(Map<String, dynamic> map) {
-    id = map[colId];
-    actHours = map[colActHours];
-    totalHours = map[colTotalHours];
-    overtimePay = map[colOvertimePay];
-    dateTime = map[colDateTime];
-    note = map[colNote];
+    _id = map[colId];
+    _actHours = map[colActHours];
+    _totalHours = map[colTotalHours];
+    _overtimePay = map[colOvertimePay];
+    _dateTime = map[colDateTime];
+    _note = map[colNote];
   }
 
   // Convert a Cat into a map. The keys must correspond to the names of the
   // columns in the database.
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
-      colActHours: actHours,
-      colTotalHours: totalHours,
-      colOvertimePay: overtimePay,
-      colDateTime: dateTime,
-      colNote: note
+      colActHours: _actHours,
+      colTotalHours: _totalHours,
+      colOvertimePay: _overtimePay,
+      colDateTime: _dateTime,
+      colNote: _note
     };
-    if (id != null) {
-      map[colId] = id;
+    if (_id != null) {
+      map[colId] = _id;
     }
     return map;
+  }
+
+  get note => _note;
+
+  set note(value) {
+    _note = value;
+  }
+
+  get dateTime => _dateTime;
+
+  set dateTime(value) {
+    _dateTime = value;
+  }
+
+  get overtimePay => _overtimePay;
+
+  set overtimePay(value) {
+    _overtimePay = value;
+  }
+
+  get totalHours => _totalHours;
+
+  set totalHours(value) {
+    _totalHours = value;
+  }
+
+  get actHours => _actHours;
+
+  set actHours(value) {
+    _actHours = value;
+  }
+
+  get id => _id;
+
+  set id(value) {
+    _id = value;
   }
 }
